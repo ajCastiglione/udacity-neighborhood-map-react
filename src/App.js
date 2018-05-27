65adee4aa7dc65c0ee023222addc84f2fde7c15a
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-05-27 19:40:10 -0400

    moved places to another file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
 import React, {Component} from 'react';
+import {places} from './components/PointsOfInterest'
 import Locations from './components/Locations';
 
 export default class App extends Component {
 
     state = {
-        POIs: [
-            {
-                'name': "Brookfield Country Club",
-                'type': "Country Club",
-                'latitude': 42.983928,
-                'longitude': -78.666017,
-                'streetAddress': "5120 Shimerville Rd, Clarence, NY 14031"
-            },
-            {
-                'name': "Clarence Public Library",
-                'type': "Library",
-                'latitude': 42.9950515,
-                'longitude': -78.6407332,
-                'streetAddress': "3 Town Pl, Clarence, NY 14031"
-            },
-            {
-                'name': "Premier Place",
-                'type': "Store",
-                'latitude': 42.98435509999999,
-                'longitude': -78.6984052,
-                'streetAddress': "7980 Transit Rd, Williamsville, NY 14221"
-            },
-            {
-                'name': "Platinum Health & Fitness",
-                'type': "Gym",
-                'latitude': 43.026843,
-                'longitude': -78.69798329999999,
-                'streetAddress': "4685 Transit Rd, Buffalo, NY 14221"
-            },
-            {
-                'name': "Santora's Pizza Pub & Grill",
-                'type': "Restaurant",
-                'latitude': 42.980571,
-                'longitude': -78.6977952,
-                'streetAddress': "7800 Transit Rd, Williamsville, NY 14221"
-            }
-        ],
+        POIs: places,
         map: '',
         infowindow: '',
         prevmarker: ''
